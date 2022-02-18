2f24f2161a333bb01a29bcec53c1c9705309a4c9
Harish <99025493+Harishvinayagam@users.noreply.github.com>
2022-02-18 20:24:09 +0530

    Add files via upload

---
+++ decreasing order of frequency.py
@@ -0,0 +1,11 @@
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+print("ENTER A WORD:")
+string=input()
+print (most_frequent(string))
